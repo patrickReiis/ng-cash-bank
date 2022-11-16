b3b088748e4c48be658b1d8b20ad6055397e3043
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-16 12:10:14 -0300

    Basic express setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,11 @@
-console.log('testing')
+import express, { Application } from 'express';
+
+export const app:Application = express();
+
+// middleware
+app.use(express.json()); // parses incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })) // parses incoming requests with urlencoded payloads
+
+app.get('/', (req, res) => {
+    res.end('Hello world');
+});
