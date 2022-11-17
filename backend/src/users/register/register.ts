632df34ab2f01efac73dbd342605762a6599de40
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-17 10:00:43 -0300

    Calling register validation in endpoint

--- backend/src/users/register/register.ts
+++ backend/src/users/register/register.ts
@@ -1,6 +1,14 @@
-import "reflect-metadata"
 import { Request, Response } from 'express';
+import { getAllRegisterValidation } from './validation'; 
 
 export async function handleRegister(req: Request, res: Response) {
-    res.end('hello from register')
+    const errors = await getAllRegisterValidation(req.body);
+
+    if (errors.length > 0) {
+        res.writeHead(400, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({errors: errors}));
+        return
+    }
+
+    res.end('register fine');
 }
