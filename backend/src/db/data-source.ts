a2d8d6894108a9a0423da4ba74b5c0c33f5c1ff1
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-18 11:38:55 -0300

    Database ORM config

---
+++ backend/src/db/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { Account } from './entity/Account';
+import { User } from './entity/User';
+
+export const dataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'ng_cash',
+    synchronize: true,
+    logging: false,
+    entities: [Account, User],
+    subscribers: [],
+    migrations: [],
+})
+
