647997329c4bfdb9812d71cdba5fad6fc8de6cd8
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-18 17:24:45 -0300

    Users can be created through REST API

--- backend/src/users/register/register.ts
+++ backend/src/users/register/register.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
 import { getAllRegisterValidation } from './validation'; 
+import { dataSource } from '../../db/get-data-source';
+import { User } from '../../db/entity/User';
+import { Account } from '../../db/entity/Account';
+import { getPasswordHashed } from '../password/password';
+import { arePasswordsEqual } from '../password/password';
 
 export async function handleRegister(req: Request, res: Response) {
     const errors = await getAllRegisterValidation(req.body);
@@ -9,6 +14,18 @@ export async function handleRegister(req: Request, res: Response) {
         res.end(JSON.stringify({errors: errors}));
         return
     }
+    
 
-    res.end('register fine');
+    // creating user
+    const user = new User();
+    user.username = req.body.username;
+    user.password = await getPasswordHashed(req.body.password);
+    // creating account for user
+    user.account = new Account();
+
+    // saving all
+    await dataSource.manager.save(user);
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ success: `${user.username}, your account has been created successfully`}));
 }
