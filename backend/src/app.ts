620431cf13fe406dd5bbb31deeb518eeb63f2969
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-21 11:12:41 -0300

    Login route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express, { Application } from 'express';
 import { handleRegister } from './users/register/register';
+import { handleLogin } from './users/login/login';
 
 export const app:Application = express();
 
@@ -10,3 +11,5 @@ app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(express.urlencoded({ extended: true })) // parses incoming requests with urlencoded payloads
 
 app.post('/api/v1/register', handleRegister);
+
+app.post('/api/v1/login', handleLogin);
