c9de881a78da46f010bd9ea0ce5d7369ee549a06
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-21 14:12:03 -0300

    Cookie parser middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,14 @@ dotenv.config();
 import express, { Application } from 'express';
 import { handleRegister } from './users/register/register';
 import { handleLogin } from './users/login/login';
+import cookieParser from 'cookie-parser';
 
 export const app:Application = express();
 
 // middleware
 app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(express.urlencoded({ extended: true })) // parses incoming requests with urlencoded payloads
+app.use(cookieParser()) // automatically formats cookies
 
 app.post('/api/v1/register', handleRegister);
 
