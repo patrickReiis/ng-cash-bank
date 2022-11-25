bb51a3e76f1a8e04c902ff0858e56e4d4ef60634
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-25 11:49:25 -0300

    Func implementation for getting all transactions history

--- backend/src/utils/transactionsHistory.ts
+++ backend/src/utils/transactionsHistory.ts
@@ -63,3 +63,49 @@ export async function handleShowCashOut(req: Request, res: Response) {
 
     res.json(transactions)
 }
+
+export async function handleShowAll(req: Request, res: Response) {
+    const token = req.cookies['token'];
+
+    const username = (jwt.decode(token) as any).username;
+    const user = await dataSource.getRepository(User).findOne({where: {username: username}, relations: {account: true}});
+
+    // I call a function that validates the JWT before calling this function
+    // if for some reason someone bypass the function that checks for authorization
+    // I return a server error (500)
+    if (user === null) {
+        res.writeHead(500);
+        res.end();
+        return
+    }
+
+    const transactionsCashIn = await dataSource.getRepository(Transactions).find({ 
+        select: {
+            value: true,
+            createdAt: true,
+        },
+        where: {
+            debitedAccountId: { 
+                id: user.account.id 
+            }
+        } 
+    });
+
+    const transactionsCashOut = await dataSource.getRepository(Transactions).find({ 
+        select: {
+            value: true,
+            createdAt: true,
+        },
+        where: {
+            creditedAccountId: { 
+                id: user.account.id
+            }
+        }
+         
+    })
+
+    res.json({
+        cashIn: transactionsCashIn,
+        cashOut: transactionsCashOut
+    })
+}
